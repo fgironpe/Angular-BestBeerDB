6fdc4610142cfe96afcb01e99bbd701323262e06
fgiron <fgironpe@gmail.com>
2023-09-21 21:40:05 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SharedModule } from './shared/shared.module';
+import { NameFilterComponent } from './pages/home/name-filter/name-filter.component';
+import { BeerCardComponent } from './pages/home/beer-card/beer-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NameFilterComponent,
+    BeerCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/home/beer-card/beer-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Beer } from 'src/app/core/models/beer.model';
+
+@Component({
+  selector: 'app-beer-card',
+  templateUrl: './beer-card.component.html',
+  styleUrls: ['./beer-card.component.scss']
+})
+export class BeerCardComponent {
+  @Input() beer: Beer = { id: 0, name: '', abv: 0, image_url: '' };
+
+  constructor() { }
+}
